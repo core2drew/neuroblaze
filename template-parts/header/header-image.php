598f092ffbbc5bf74c42d02f13147da4fd3e6057
core2drew <calupesandro@gmail.com>
2018-06-03 09:06:27 +0800

    Update header text

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -1,7 +1,11 @@
 <div class="header">
 
   <div class="info">
-    <h1>Hello World</h1>
+    <h1 class="title">Lorem ipsum dolor sit ame</h1>
+    <p class="description">consectetur adipiscing elit. Sed tristique feugiat metus. 
+    Sed et erat et odio lobortis dictum. Aenean maximus 
+    nunc egestas, dapibus erat non, rutrum mauris</p>
+    <span class="underline"></span>
   </div>
   <!-- <a href="#start" id="section06"><span></span></a> -->
 </div>
