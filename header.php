66d467196eacf7fe403f5fe8968baee386b7185a
core2drew <calupesandro@gmail.com>
2018-08-09 01:23:56 +0800

    Remove owl slider and enable cdn bootstrap

--- header.php
+++ header.php
@@ -15,13 +15,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href=<?php echo get_stylesheet_directory_uri() . "/assets/images/torch.ico" ?> type="image/ico" sizes="16x16">
-<link href=<?php echo get_stylesheet_directory_uri() . "/assets/bootstrap/css/bootstrap.min.css" ?> rel="stylesheet">
-<link href=<?php echo get_stylesheet_directory_uri() . "/assets/owlcarousel/assets/owl.carousel.min.css" ?> rel="stylesheet">
-<link href=<?php echo get_stylesheet_directory_uri() . "/assets/owlcarousel/assets/owl.theme.default.min.css" ?> rel="stylesheet">
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-<!-- <link rel="profile" href="http://gmpg.org/xfn/11"> -->
-<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> -->
-<!-- <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
